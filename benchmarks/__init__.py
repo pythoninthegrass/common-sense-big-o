b2d1b5747f775924867580069bf33a473556a059
Matt Winfield <matthew.r.winfield@faa.gov>
2025-12-24 22:06:26 -0600

    Added all functions to module init

--- benchmarks/__init__.py
+++ benchmarks/__init__.py
@@ -1,3 +1,4 @@
-from .chapter_01 import PrintNumbersVersionOneBenchmark
+from .chapter_01 import PrintNumbersVersionOneBenchmark, PrintNumbersVersionTwoBenchmark, LinearSearch
+from .chapter_02 import BinarySearch
 from .chapter_04 import BubbleSort
 from .chapter_05 import SelectionSort
